c44c0bf69e9ef4c467c6b5c9ab8cacc12c981a92
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-05 22:53:59 -0800

    basic time series analysis exploratory plots and dumping to pdf

--- explore.R
+++ explore.R
@@ -12,6 +12,10 @@ data_file = "lai_Lmon_CCSM4_rcp45_r2i1p1_200601-210012.nc"
 data_path = paste(data_dir, data_file, sep="")
 attr_name = "lai" # leaf area index
 plot.bool = T # plot results along the way?
+plot.filename = "basic_analysis.pdf"
+
+# Open outfile in write mode
+pdf(file=plot.filename)
 
 # Load the.nc
 ncin = nc_open(data_path)
@@ -69,5 +73,84 @@ st.day = 1 # starting day, 1 is Jan 01, 2005
 # holds the times of each observation
 day.ixs = seq(from=st.day, to=length(d.af), by=365)
 
+## Standardize variance, detrend, deseason etc. standard time series analysis
+
+# Check acf and pacf
+if(plot.bool){
+    par(mfrow=c(2,1))
+    acf(d.af, main="Raw data (ACF)")
+    pacf(d.af, main="Raw data (PACF)")
+}
+
+# detrend with polynomial model
+df = data.frame(obs=d.af, time=t)
+degree = 6
+fit.lm = lm(obs ~ poly(time, degree, raw=TRUE), df)
+d.af.detr = d.af-predict(fit.lm)
+
+if(plot.bool){
+    par(mfrow=c(3,1))
+    plot(t, predict(fit.lm),
+         xlab="Time", ylab="Polynomial signal",
+         type="l")
+    title(main="Polynomial trend")
+
+    plot(t, d.af,
+         xlab="Time", ylab="True signal",
+         type="l", col="red")
+    title(main="Observations")
+
+    plot(t, d.af.detr,
+         xlab="Time", ylab="Detrended signal",
+         type="l", col="blue")
+    title(main="Detrended")
+    par(mfrow=c(1,1))
+}
+
+# Check acf and pacf with detrended data
+if(plot.bool){
+    par(mfrow=c(2,1))
+    acf(d.af.detr, main="Detrended data (ACF)")
+    pacf(d.af.detr, main="Detrended data (PACF)")
+}
+
+# try using differences
+d.af.diff = diff(d.af)
+if(plot.bool){
+    par(mfrow=c(2,1))
+    acf(d.af.diff, main="Differenced data (ACF)")
+    pacf(d.af.diff, main="Differenced data (PACF)")
+}
+
+# moving average to de-season
+filter.length = 12 # 12 used because t[1]-t[13] = -365
+filt.ma = filter(d.af.detr, rep(1,filter.length)/filter.length)
+if(plot.bool){
+    par(mfrow=c(1,1))
+    plot(t, d.af.detr, type="l", xlab="Days since 01-01-2005", ylab="LAI")
+    lines(t, filt.ma, type="l", col="red")
+    title(main="One year seasonality removal from detrended observations")
+}
+
+d.af.deseas = d.af.detr - filt.ma
+if(plot.bool){
+    par(mfrow=c(3,1))
+    plot(t, d.af.deseas,
+         type="l", col="green",
+         xlab="Days since 01-01-2005", ylab="Deseasoned signal",
+         main="Deseasoned LAI"
+         )
+    acf(na.omit(d.af.deseas))
+    pacf(na.omit(d.af.deseas))
+}
+
+# TODO: write all plots out to pdf
+# TODO: detrend using filter(12), deseason with filter(3)
+# TODO: try polynomial deseasoning
+
+# Close graphics output file lock
+dev.off()
+
 # SOURCES:
 # 1. http://geog.uoregon.edu/GeogR/topics/netCDF-read-ncdf4.html
+# 2. https://rpubs.com/ryankelly/ts2
