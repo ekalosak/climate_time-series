37b13c15fe22650eb78807800f29774eb71b4fb4
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-09 22:36:54 -0800

    improved plots; added spectral analysis to deseasoned data

--- explore.R
+++ explore.R
@@ -12,8 +12,10 @@ data_dir = "./data/"
 data_file = "lai_Lmon_CCSM4_rcp45_r2i1p1_200601-210012.nc"
 data_path = paste(data_dir, data_file, sep="")
 attr_name = "lai" # leaf area index
-plot.bool = T # plot results along the way?
 plot.filename = "basic_analysis.pdf"
+
+# plotting setup
+par(lwd=2)
 
 # Open outfile in write mode
 pdf(file=plot.filename)
@@ -47,26 +49,22 @@ z = d[,,1] # all LAI values across the planet at time 1
 
 # plotting subroutines
 leaf.colors = function(x){rev(terrain.colors(x))}
-if(plot.bool){
-    filled.contour(x,y,z, color = leaf.colors, asp = 1,
-                   plot.axes={
-                       axis(1); axis(2);
-                       points(
-                              floor(lon[loc.af[2]]),
-                              floor(lat[loc.af[1]])
-                              )
-                   })
-    title(xlab="Longitude", ylab="Latitude", main="LAI at T=1")
-}
+filled.contour(x,y,z, color = leaf.colors, asp = 1,
+               plot.axes={
+                   axis(1); axis(2);
+                   points(
+                          floor(lon[loc.af[2]]),
+                          floor(lat[loc.af[1]])
+                          )
+               })
+title(xlab="Longitude", ylab="Latitude", main="Leaf area, Jan 2005")
 
 ## Extract the data for the time series in a single location in sub-saharan
 ## Africa (i.e. single v=LAI, single s=African, all T=2000 to 2100)
 d.af = d[loc.af[2],loc.af[1],] # d.af is african single loc data from 2000 to 2100
-if(plot.bool){
-    plot(t, d.af, xlab="Time", ylab="Tree cover (LAI)",
-          main="Single location sub-Saharan African tree cover over a century",
-          type="l")
-}
+plot(t, d.af, xlab="Time", ylab="Tree cover (LAI)",
+      main="Single location sub-Saharan African tree cover over a century",
+      type="l")
 
 ## Extract a single day each year as an attempt to de-season the data
 st.day = 1 # starting day, 1 is Jan 01, 2005
@@ -77,11 +75,9 @@ day.ixs = seq(from=st.day, to=length(d.af), by=365)
 ## Standardize variance, detrend, deseason etc. standard time series analysis
 
 # Check acf and pacf
-if(plot.bool){
-    par(mfrow=c(2,1))
-    acf(d.af, main="Raw data (ACF)")
-    pacf(d.af, main="Raw data (PACF)")
-}
+par(mfrow=c(2,1))
+acf(d.af, main="Raw data (ACF)")
+pacf(d.af, main="Raw data (PACF)")
 
 # detrend with polynomial model
 df = data.frame(obs=d.af, time=t)
@@ -89,69 +85,78 @@ degree = 6
 fit.lm = lm(obs ~ poly(time, degree, raw=TRUE), df)
 d.af.detr = d.af-predict(fit.lm)
 
-if(plot.bool){
-    par(mfrow=c(3,1))
-    plot(t, predict(fit.lm),
-         xlab="Time", ylab="Polynomial signal",
-         type="l")
-    title(main="Polynomial trend")
+par(mfrow=c(2,1))
+plot(t, predict(fit.lm),
+     xlab="Time", ylab="Polynomial signal",
+     type="l", col="purple",
+     ylim=range(d.af))
+title(main="Polynomial trend")
+lines(t, d.af)
 
-    plot(t, d.af,
-         xlab="Time", ylab="True signal",
-         type="l", col="red")
-    title(main="Observations")
-
-    plot(t, d.af.detr,
-         xlab="Time", ylab="Detrended signal",
-         type="l", col="blue")
-    title(main="Detrended")
-    par(mfrow=c(1,1))
-}
+plot(t, d.af.detr,
+     xlab="Time", ylab="Detrended signal",
+     type="l", col="blue")
+title(main="Detrended")
+par(mfrow=c(1,1))
 
 # Check acf and pacf with detrended data
-if(plot.bool){
-    par(mfrow=c(2,1))
-    acf(d.af.detr, main="Detrended data (ACF)")
-    pacf(d.af.detr, main="Detrended data (PACF)")
-}
+par(mfrow=c(2,1))
+acf(d.af.detr, main="Detrended data (ACF)")
+pacf(d.af.detr, main="Detrended data (PACF)")
 
 # try using differences
 d.af.diff = diff(d.af)
-if(plot.bool){
-    par(mfrow=c(2,1))
-    acf(d.af.diff, main="Differenced data (ACF)")
-    pacf(d.af.diff, main="Differenced data (PACF)")
-}
+par(mfrow=c(2,1))
+acf(d.af.diff, main="Differenced data (ACF)")
+pacf(d.af.diff, main="Differenced data (PACF)")
 
 # moving average to de-season
 filter.length = 12 # 12 used because t[1]-t[13] = -365
 filt.ma = filter(d.af.detr, rep(1,filter.length)/filter.length)
-if(plot.bool){
-    par(mfrow=c(1,1))
-    plot(t, d.af.detr, type="l", xlab="Days since 01-01-2005", ylab="LAI")
-    lines(t, filt.ma, type="l", col="red")
-    title(main="One year seasonality removal from detrended observations")
-}
+par(mfrow=c(1,1))
+plot(t, d.af.detr, type="l", xlab="Days since 01-01-2005", ylab="LAI")
+lines(t, filt.ma, type="l", col="red")
+title(main="Moving average seasonality removal")
 
 #d.af.deseas = d.af.detr - filt.ma
 d.af.deseas = filt.ma
-if(plot.bool){
-    par(mfrow=c(3,1))
-    plot(t, d.af.deseas,
-         type="l", col="green",
-         xlab="Days since 01-01-2005", ylab="Deseasoned signal",
-         main="Deseasoned LAI"
-         )
-    acf(na.omit(d.af.deseas))
-    pacf(na.omit(d.af.deseas))
-}
+par(mfrow=c(3,1))
+plot(t, d.af.deseas,
+     type="l", col="green",
+     xlab="Days since 01-01-2005", ylab="Deseasoned signal",
+     main="Deseasoned LAI"
+     )
+acf(na.omit(d.af.deseas))
+pacf(na.omit(d.af.deseas))
 
 # TODO: detrend using filter(12), deseason with filter(3)
 # TODO: try polynomial deseasoning
 # TODO: generate resampled CI from..?
 
-# Fit arima(0,1,1) as a first pass to the deseasoned data
-mod1 = arima(d.af.deseas, c(0,1,1))
+## Fit time series models
+# m = arima(d.af.deseas, c(0,1,1))
+m = ar(na.omit(d.af.deseas), order.max=4) # looking at the PACF
+
+## simulate some using the model to spot check
+ts = m$ar
+s = arima.sim(list(order=c(4,0,0), ar=ts), n=length(t))
+par(mfrow=c(2,1))
+plot(1:length(s), s,
+     main="Simulated from AR(4) ~ deseasoned",
+     xlab="Months (1140mo=95yr)", ylab="LAI",
+     type="l", col="blue")
+plot(t, d.af.deseas,
+     main="Deseasoned data",
+     xlab="Days (3500d=96yr i.e. 2005-2100)", ylab="LAI",
+     type="l", col="green")
+
+## spectral analysis
+par(mfrow=c(2,1))
+plot(1:length(d.af.deseas), d.af.deseas,
+     type="l", main="Deseasoned observations", col="green")
+sp = spec.ar(m, main="Spectral density of AR(4)",
+             col="orange")
+par(mfrow=c(1,1))
 
 # Close graphics output file lock
 dev.off()
