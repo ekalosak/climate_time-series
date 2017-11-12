6535164b0ab371aec01033e3a48b7eb612861a2b
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-11 21:40:48 -0800

    renamed Amy's pca analysis file

--- src/pcafpca.R
+++ src/pcafpca.R
@@ -1,7 +1,12 @@
-#
+# Author: Amy
+# Performs FPCA anaalysis
+
 library(fdapace)
+
+# load data
 data2 = read.csv("leafidx.csv")
-#Take alook to see how we can fit a time-series model.
+
+# Take a look to see how we can fit a time-series model.
 test1 = which(data2[,1] == 26.25) #logitude
 plot(1:1140, data2[test1[1],-(1:2)], ylim = c(0, 20), type = 'l', col = "grey")
 temp = sapply(2:15, function(i) lines(1:1140, data2[test1[i],-(1:2)], col = "grey"))
@@ -22,12 +27,13 @@ pc1 = prcomp(data2[,-(1:2)])
 plot(pc1)
 screeplot(pc1, npcs = 13)
 dim(pc1$x)
-fit1 = arima(pc1$x[,1]) 
+fit1 = arima(pc1$x[,1])
 fit1
 
 # fPCA
 library(fdapace)
-tmplist = MakeFPCAInputs(ID = rep(1:dim(data2)[1], each = 1140), tVec = rep(1:1140, by = dim(data)[1]), 
+tmplist = MakeFPCAInputs(ID = rep(1:dim(data2)[1], each = 1140),
+                         tVec = rep(1:1140, by = dim(data)[1]),
                          y = as.vector(t(data2[,-(1:2)])))
 fpca1 = FPCA(tmplist$Ly, tmplist$Lt, optns = list(dataType = "Dense"))
 fpca1$xiEst
