d5ec9f301fb1ee67786bad42bf6669c9095660e2
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-11 23:37:21 -0800

    better detrending (using higher degree polynomial

--- src/detrend.R
+++ src/detrend.R
@@ -1,30 +1,30 @@
 # Author: Eric
 # Detrend raw Leaf Area Index observations
 
-source("load_data.R", echo=TRUE)
+library(TTR) # runSD() - running standard dev
 
-#Setup output file name
-plot.filename = "../pdf/detrend_fit_ARIMA.pdf"
+source("load_data.R") #, echo=TRUE)
 
-# Open outfile in write mode
-pdf(file=plot.filename)
-
-## Extract a single day each year as an attempt to de-season the data
-st.day = 1 # starting day, 1 is Jan 01, 2005
-# NOTE: the following doesn't work... need to go throught the "t" vector that
-# holds the times of each observation
-day.ixs = seq(from=st.day, to=length(d.af), by=365)
+## Extract a single month each year as an attempt to de-season the data
+st.mo = 1 # starting month, 1 is Jan 01, 2005
+mo.ixs = seq(from=st.mo, to=length(d.af), by=12)
+# TODO: try this single season deseasonalization
+# TODO: deseasonalize into 12 potentially covarrying timeserieses
 
 ## Standardize variance, detrend, deseason etc. standard time series analysis
 
 # detrend with polynomial model
 df = data.frame(obs=d.af, time=t)
-degree = 6
+degree = 15
 fit.lm = lm(obs ~ poly(time, degree, raw=TRUE), df)
-d.af.detr = d.af-predict(fit.lm)
+d.af.detr.nosd = d.af - predict(fit.lm)
+# d.af.detr = d.af-predict(fit.lm)
 
 # standardize variance
 rsd = runSD(d.af.detr.nosd, 12*10) # 10 year window for sd calculation
+msd = lm(rsd~t)
+msd.p = predict.lm(msd, data.frame(t))
+d.af.detr = d.af.detr.nosd/msd.p
 
 # try using differences
 d.af.diff = diff(d.af)
@@ -39,9 +39,5 @@ d.af.deseas.resid = d.af.detr - filt.ma
 # TODO: detrend using filter(12), deseason with filter(3)
 # TODO: try polynomial deseasoning
 
-# Close graphics output file lock
-graphics.off()
-
 # SOURCES:
-# 1. http://geog.uoregon.edu/GeogR/topics/netCDF-read-ncdf4.html
-# 2. https://rpubs.com/ryankelly/ts2
+# 1. https://rpubs.com/ryankelly/ts2
