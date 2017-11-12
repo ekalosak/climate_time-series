be9fa303d69bb3198bf05ad9f6fa404fdc64a9ff
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-11 22:29:32 -0800

    product of refactoring explore.R

--- src/basic_ts_analysis.R
+++ src/basic_ts_analysis.R
@@ -8,12 +8,15 @@ library(ncdf4) # load .nc data files
 library(stats) # arima()
 library(TTR) # runSD() - running standard dev
 
+source("load_data.R")
+source("detrend.R")
+
 # Setup parameters
 data_dir = "./data/"
 data_file = "lai_Lmon_CCSM4_rcp45_r2i1p1_200601-210012.nc"
 data_path = paste(data_dir, data_file, sep="")
 attr_name = "lai" # leaf area index
-plot.filename = "basic_analysis.pdf"
+plot.filename = "../pdf/basic_analysis.pdf"
 
 # plotting setup
 par(lwd=2)
@@ -21,49 +24,34 @@ par(lwd=2)
 # Open outfile in write mode
 pdf(file=plot.filename)
 
-# Load the.nc
-ncin = nc_open(data_path)
-
-# Extract lat, lon, time
-lon = ncvar_get(ncin, "lon")
-lat = ncvar_get(ncin, "lat")
-loc.af = c(
-           min(which(lat > 3)),
-           min(which(lon > 25))
-           )
-t = ncvar_get(ncin, "time")
-tunits = ncatt_get(ncin, "time", "units")
-
-# Extract attribute and data
-attr = ncatt_get(ncin, attr_name)
-attr_data = ncvar_get(ncin, attr_name)
-d = attr_data # shorthand for the array of space X time
-# NOTE: d is indexed d[lon, lat, time], but the natural location indexing is
-# loc = [lat, lon] so to index d by a location you must do
-# d[loc[2], loc[1], time] rather than d[loc[1], loc[2], time]
-
 ## Plot the global LAI at time 0
 # extract variables to plot
 x = lon # 288 longitudes
 y = lat # 192 latitudes
 z = d[,,1] # all LAI values across the planet at time 1
 
-# plotting subroutines
 leaf.colors = function(x){rev(terrain.colors(x))}
-filled.contour(x,y,z, color = leaf.colors,
+filled.contour(x,y,z, color = leaf.colors, asp = 1,
                plot.axes={
                    axis(1); axis(2);
                    points(
                           floor(lon[loc.af[2]]),
-                          floor(lat[loc.af[1]]),
-                          col="purple", lwd=3
+                          floor(lat[loc.af[1]])
                           )
                })
-title(xlab="Longitude", ylab="Latitude", main="Leaf area, Jan 2005")
+title(xlab="Longitude", ylab="Latitude", main="LAI at T=1")
 
-## Extract the data for the time series in a single location in sub-saharan
+plot(t, d.af, xlab="Time", ylab="Tree cover (LAI)",
+      main="Single location sub-Saharan African tree cover over a century",
+      type="l")
+
+# acf and pacf
+par(mfrow=c(2,1))
+acf(d.af, main="Raw data (ACF)")
+pacf(d.af, main="Raw data (PACF)")
+
+## Plot the data for the time series in a single location in sub-saharan
 ## Africa (i.e. single v=LAI, single s=African, all T=2000 to 2100)
-d.af = d[loc.af[2],loc.af[1],] # d.af is african single loc data from 2000 to 2100
 plot(t, d.af, xlab="Time", ylab="Tree cover (LAI)",
       main="Single location sub-Saharan African tree cover over a century",
       type="l")
@@ -98,8 +86,12 @@ plot(t, d.af.detr.nosd,
      type="l", col="blue")
 title(main="Detrended")
 
-# standardize variance
-rsd = runSD(d.af.detr.nosd, 12*10) # 10 year window for sd calculation
+# Check acf and pacf with detrended data
+par(mfrow=c(2,1))
+acf(d.af.detr, main="Detrended data (ACF)")
+pacf(d.af.detr, main="Detrended data (PACF)")
+
+# Plot standardized variance data
 plot(t, rsd, main="Running standard deviation 10 year window",
      xlab="Days since Jan 2005", ylab="Std Dev",
      type="l")
@@ -116,8 +108,7 @@ plot(t, d.af.detr,
 acf(d.af.detr, main="Detrended data (ACF)")
 pacf(d.af.detr, main="Detrended data (PACF)")
 
-# try using differences
-d.af.diff = diff(d.af)
+# Plot differenced data
 plot(t, d.af.detr, type="l", col="blue",
      main="Detrended", xlab="Days since 01-01-2005", ylab="LAI")
 plot(t[1:length(d.af.diff)], d.af.diff, type="l", col="red",
@@ -125,12 +116,11 @@ plot(t[1:length(d.af.diff)], d.af.diff, type="l", col="red",
 acf(d.af.diff, main="Differenced data (ACF)")
 pacf(d.af.diff, main="Differenced data (PACF)")
 
-# moving average to de-season
-filter.length = 12 # 12 used because t[1]-t[13] = -365
-filt.ma = filter(d.af.detr, rep(1,filter.length)/filter.length)
-
-#d.af.deseas = d.af.detr - filt.ma
-d.af.deseas = filt.ma
+# Plot the moving average deseasonalization
+par(mfrow=c(1,1))
+plot(t, d.af.detr, type="l", xlab="Days since 01-01-2005", ylab="LAI")
+lines(t, filt.ma, type="l", col="red")
+title(main="One year seasonality removal from detrended observations")
 
 plot(t, d.af.detr, type="l",
      xlab="Days since 01-01-2005", ylab="LAI", col="blue")
@@ -138,6 +128,8 @@ lines(t, filt.ma, type="l", col="red")
 title(main="Moving average seasonality (filter.width=1yr)")
 abline(v=t[120])
 abline(v=t[132]) # show a year width on plot
+
+par(mfrow=c(3,1))
 plot(t, d.af.deseas,
      type="l", col="green",
      xlab="Days since Jan, 2005", ylab="Deseasoned signal",
@@ -145,7 +137,9 @@ plot(t, d.af.deseas,
 
 acf(na.omit(d.af.deseas))
 pacf(na.omit(d.af.deseas))
-par(mfrow=c(2,1))
+
+# Fit arima(0,1,1) as a first pass to the deseasoned data
+mod1 = arima(d.af.deseas, c(0,1,1))
 
 ## spectral analysis and sinusoidal fit to deseason
 par(mfrow=c(1,1))
@@ -182,6 +176,4 @@ sp = spec.ar(m, main="Spectral density of AR(4)",
 graphics.off()
 
 # SOURCES:
-# 1. http://geog.uoregon.edu/GeogR/topics/netCDF-read-ncdf4.html
-# 2. https://rpubs.com/ryankelly/ts2
-
+# 1. https://rpubs.com/ryankelly/ts2
