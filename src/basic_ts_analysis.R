64abd5895e7ac36e5c20eb12c639c6d6236a7b0e
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-11 23:38:03 -0800

    removed plotting and processing redundancies

--- src/basic_ts_analysis.R
+++ src/basic_ts_analysis.R
@@ -4,31 +4,26 @@
 
 rm(list=ls()) # clean workspace
 
-library(ncdf4) # load .nc data files
 library(stats) # arima()
-library(TTR) # runSD() - running standard dev
 
-source("load_data.R")
-source("detrend.R")
+source("detrend.R") # loads data and detrends, stds variance, etc.
 
-# Setup parameters
-data_dir = "./data/"
-data_file = "lai_Lmon_CCSM4_rcp45_r2i1p1_200601-210012.nc"
-data_path = paste(data_dir, data_file, sep="")
-attr_name = "lai" # leaf area index
+# Setup parameters (in load_data.R)
+# data_dir = "../data/"
+# data_file = "lai_Lmon_CCSM4_rcp45_r2i1p1_200601-210012.nc"
+# data_path = paste(data_dir, data_file, sep="")
+# attr_name = "lai" # leaf area index
+
+# Plotting setup
 plot.filename = "../pdf/basic_analysis.pdf"
-
-# plotting setup
-par(lwd=2)
-
-# Open outfile in write mode
 pdf(file=plot.filename)
+par(lwd=2)
 
 ## Plot the global LAI at time 0
 # extract variables to plot
 x = lon # 288 longitudes
 y = lat # 192 latitudes
-z = d[,,1] # all LAI values across the planet at time 1
+z = d[,,7] # all LAI values across the planet at time 7 (July 2005)
 
 leaf.colors = function(x){rev(terrain.colors(x))}
 filled.contour(x,y,z, color = leaf.colors, asp = 1,
@@ -39,72 +34,45 @@ filled.contour(x,y,z, color = leaf.colors, asp = 1,
                           floor(lat[loc.af[1]])
                           )
                })
-title(xlab="Longitude", ylab="Latitude", main="LAI at T=1")
-
-plot(t, d.af, xlab="Time", ylab="Tree cover (LAI)",
-      main="Single location sub-Saharan African tree cover over a century",
-      type="l")
-
-# acf and pacf
-par(mfrow=c(2,1))
-acf(d.af, main="Raw data (ACF)")
-pacf(d.af, main="Raw data (PACF)")
+title(xlab="Longitude", ylab="Latitude", main="Global leaf cover, July 2005")
 
 ## Plot the data for the time series in a single location in sub-saharan
 ## Africa (i.e. single v=LAI, single s=African, all T=2000 to 2100)
-plot(t, d.af, xlab="Time", ylab="Tree cover (LAI)",
-      main="Single location sub-Saharan African tree cover over a century",
-      type="l")
-
-## Extract a single day each year as an attempt to de-season the data
-st.day = 1 # starting day, 1 is Jan 01, 2005
-# NOTE: the following doesn't work... need to go throught the "t" vector that
-# holds the times of each observation
-day.ixs = seq(from=st.day, to=length(d.af), by=365)
-
-## Standardize variance, detrend, deseason etc. standard time series analysis
-
-# Check acf and pacf
-par(mfrow=c(2,1))
+par(mfrow=c(3,1))
+plot(t, d.af,
+     xlab="Days since Jan 2005",
+     ylab="Tree cover (LAI)",
+     main="Sub-Saharan Africa, 2005-2100",
+     type="l")
 acf(d.af, main="Raw data (ACF)")
 pacf(d.af, main="Raw data (PACF)")
 
-# detrend with polynomial model
-df = data.frame(obs=d.af, time=t)
-degree = 6
-fit.lm = lm(obs ~ poly(time, degree, raw=TRUE), df)
-d.af.detr.nosd = d.af-predict(fit.lm)
+## Plot standardize variance, detrend, deseasoned data
 
+# plot detrended data
+par(mfrow=c(2,1))
 plot(t, predict(fit.lm),
      xlab="Time", ylab="Polynomial signal",
      type="l", col="purple",
      ylim=range(d.af))
-title(main="Polynomial trend")
+title(main=paste("Polynomial trend (poly.degree=", degree, ")", sep=""))
 lines(t, d.af)
 plot(t, d.af.detr.nosd,
      xlab="Time", ylab="Detrended signal",
      type="l", col="blue")
 title(main="Detrended")
 
-# Check acf and pacf with detrended data
-par(mfrow=c(2,1))
-acf(d.af.detr, main="Detrended data (ACF)")
-pacf(d.af.detr, main="Detrended data (PACF)")
-
-# Plot standardized variance data
+# plot standardized variance data
 plot(t, rsd, main="Running standard deviation 10 year window",
      xlab="Days since Jan 2005", ylab="Std Dev",
      type="l")
-msd = lm(rsd~t)
-msd.p = predict.lm(msd, data.frame(t))
 lines(t, msd.p)
-d.af.detr = d.af.detr.nosd/msd.p
 plot(t, d.af.detr,
      main="Detrended obsv with stdzd SD",
      xlab="Days since Jan 2005", ylab="f(LAI)",
      type="l")
 
-# Check acf and pacf with detrended data
+# Check acf and pacf with detrended, variance standardized data
 acf(d.af.detr, main="Detrended data (ACF)")
 pacf(d.af.detr, main="Detrended data (PACF)")
 
