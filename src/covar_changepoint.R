11a62cf283da376f6fde14cf0a82c0144604ec2d
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-11-27 15:51:42 -0800

    extracting parameters from main data frame; initializing covariance time series object

--- src/covar_changepoint.R
+++ src/covar_changepoint.R
@@ -4,3 +4,14 @@
 
 rm(list=ls())
 source("detrend.R") #, echo=TRUE)
+
+lat = dim(d)[1] # TODO: check that lat=1 and lon=2, or reverse?
+lon = dim(d)[2]
+obs = dim(d)[3] # number of observations in data frame
+yr = 12 # observations are every month, so 12 of them per year
+yrs = obs/yr # number of years simulated
+
+covts = array(NA, dim=c(lat*lon, lat*lon, yrs))
+
+# TODO: CONTINUE: For each year of observations, calculate locationwise
+# covariance
