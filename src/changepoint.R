440e20ff206ff640477db52b884d71575cbf1166
Cody Carroll <cjcarroll@ucdavis.edu>
2017-12-11 17:10:31 -0800

    final version with cody's updated version

--- src/changepoint.R
+++ src/changepoint.R
@@ -13,16 +13,19 @@ ts.af=ts(d.af)
 #autoplot(ts.af, main = "Sub-Saharan Africa LAI")
 
 library(changepoint)
-cpt.af=cpt.mean(ts.af)
-?cpt.mean
+cpt.af=cpt.mean(ts.af, method = "AMOC", class=T, pen.value = 0.01)
+summary(cpt.af)
 png(filename="../img/changepoint_LAI.png")
 autoplot(cpt.af,main = "Sub-Saharan Africa LAI, Changepoint in Mean at 2026")
 dev.off()
-cpt.af=cpt.mean(ts.af, method="PELT")
-png(filename="../img/multi_changepoint_LAI.png")
-autoplot(cpt.af,main = "Sub-Saharan Africa LAI, Changepoint in Mean at 2026")
+idx1=1:257
+d.af1=d.af[-idx1]
+ts.af1=ts(d.af1)
+cpt.af1=cpt.mean(ts.af1, method = "AMOC", class=T, pen.value = 0.05)
+summary(cpt.af1)
+png(filename="../img/changepoint_LAI_seg.png")
+autoplot(cpt.af1,main = "Sub-Saharan Africa LAI, First Segment Removed")
 dev.off()
-cpt.af
 
 png(filename="../img/RawSpec_LAI.png")
 raw.spec <- spec.pgram(d.af, main="Periodogram of Raw TS", 
